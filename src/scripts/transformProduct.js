0f396e9b8d0c8509bd0edd27cafc8eb88516d19f
anvaka <anvaka@gmail.com>
2014-04-01 13:30:34 -0700

    Added price

--- src/scripts/transformProduct.js
+++ src/scripts/transformProduct.js
@@ -3,8 +3,13 @@
  */
 module.exports = function transform(papiResponse, product) {
   // let's add price if we can
-  var attributes = (papiResponse.ItemAttributes && papiResponse.ItemAttributes[0]);
-  var listPrice = (attributes.ListPrice && attributes.ListPrice[0]);
+  if (!papiResponse) return;
+  if (!papiResponse.ItemAttributes) return;
+
+  var attributes = papiResponse.ItemAttributes[0];
+  if (!attributes || !attributes.ListPrice) return;
+
+  var listPrice = attributes.ListPrice[0];
   if (listPrice && listPrice.FormattedPrice && listPrice.FormattedPrice[0]) {
     product.title = listPrice.FormattedPrice[0] + " " + product.title;
   }
